af5bc6e626aaf745d9113bf823d84115648386e2
hseleiro <hseleiro@gmail.com>
2022-09-29 17:15:33 +0100

    Lazy routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'private', loadChildren:()=>import('./private/private.module').then(module => module.PrivateModule)},
+  {path: 'public', loadChildren:()=>import('./public/public.module').then(module => module.PublicModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/public/public.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PublicRoutingModule } from './public-routing.module';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+
+@NgModule({
+  declarations: [
+    WelcomeComponent
+  ],
+  imports: [
+    CommonModule,
+    PublicRoutingModule
+  ]
+})
+export class PublicModule { }
